4fcfd44115c3e5560100f3a840bf4bb94b69cb89
DavidPascual1985 <david_siglo21@hotmail.com>
2023-04-07 03:10:40 -0300

    Agregados en 'imports' y 'exports'

--- MiPortfolio/src/app/app-routing.module.ts
+++ MiPortfolio/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './componentes/index/index.component';
 
-
+const routes: Routes = [
+  {path: '', component: IndexComponent}
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
