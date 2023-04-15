e64c7cf91b94d057b39ba3737ede9f4e27c2e3b8
DavidPascual1985 <david_siglo21@hotmail.com>
2023-04-15 02:49:39 -0300

    Importacion de Observable y HttpClient

--- MiPortfolio/src/app/servicios/portfolio.service.ts
+++ MiPortfolio/src/app/servicios/portfolio.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PortfolioService {
 
-  constructor() { }
+  constructor(private aliasUno:HttpClient) { }
+
+  obtenerDatos():Observable<any>{
+
+    return this.aliasUno.get('./assets/json/database.json');
+  }
+
 }
